914b9e5d1fbaae5b24ec118459214abe862b1ea8
Vahinkolaukausosuma <jonnisaatanaperkele@gmail.com>
2017-12-03 20:23:40 +0200

    ah thats better

--- gamemodes/cheats!/gamemode/crosshair.lua
+++ gamemodes/cheats!/gamemode/crosshair.lua
@@ -18,12 +18,9 @@ local V_customcrosshair = CreateClientConVar("Customcrosshair","1",false)
 function F_Crosshair()
 	if V_customcrosshair:GetBool() then
 		local CPos = LocalPlayer():GetEyeTrace().HitPos:ToScreen()
-		surface.SetDrawColor( 255,100,100, 255 )
+		surface.SetDrawColor( 255,100,100, 255)
 		draw.NoTexture()
-		draw.Circle(CPos.x,CPos.y,4,20)
-		surface.SetDrawColor( 255,100,100, 150 )
-		--draw.Circle(CPos.x,CPos.y,4.15,20)
+		draw.Circle(CPos.x,CPos.y,4,200)
 	end
 end
-hook.Add("HUDPaint","H_Crosshair",F_Crosshair)
-
+hook.Add("HUDPaint","Crosshairi",F_Crosshair)
