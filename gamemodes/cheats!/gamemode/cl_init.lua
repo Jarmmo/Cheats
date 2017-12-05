cb305d6fe5aedc50cedaa26a12332bd22adf9691
Vahinkolaukausosuma <jonnisaatanaperkele@gmail.com>
2017-12-06 00:13:25 +0200

    added quickswitch

--- gamemodes/cheats!/gamemode/cl_init.lua
+++ gamemodes/cheats!/gamemode/cl_init.lua
@@ -1,4 +1,5 @@
 include("shared.lua")
 include("crosshair.lua")
+include("quickswitch.lua")
 
 hook.Add("CreateClientsideRagdoll","begoneTHOT",function(entit,rag) rag:SetSaveValue( "m_bFadingOut", true ) end)
