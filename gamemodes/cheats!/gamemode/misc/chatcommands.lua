16fab30d3a856e448090309bea1eb377e46f449c
Jarmmo <jami.knaappila00@gmail.com>
2017-12-20 19:47:43 +0200

    team selection looks nice now, interactivity to voting

--- gamemodes/cheats!/gamemode/misc/chatcommands.lua
+++ gamemodes/cheats!/gamemode/misc/chatcommands.lua
@@ -3,20 +3,26 @@ for k,v in pairs(player.GetAll())do
 end
 
 hook.Add("PlayerSay","chatcommands",function(ply,text)
-	if(string.StartWith(text,"!team"))then
+	if(string.StartWith(string.lower(text),"!team"))then
 		ply:SendLua("TeamMenu()")
 		return ""
 	end
 
-	if(string.StartWith(text,"!start"))then
+	if(string.StartWith(string.lower(text),"!start"))then
 		if(GetGlobalBool("Deathmatch") and !ply:GetNWBool("Voted") and ply:Team() != 0)then
 			ply:SetNWBool("Voted",true)
 			SetGlobalInt("Voteamount",GetGlobalInt("Voteamount")+1)
 			if((100*GetGlobalInt("Voteamount")/table.Count(player.GetAll())) > 75)then
-				GameLobby()
-				SetGlobalInt("Voteamount",0)
 				for k,v in pairs(player.GetAll())do
 					v:SetNWBool("Voted",false)
+					v:ChatPrint(ply:Name().." has voted to start a game! He is the last one to vote!")
+				end
+				GameLobby()
+				SetGlobalInt("Voteamount",0)
+			else
+				for k, v in pairs(player.GetAll()) do
+					v:ChatPrint(ply:Name().." as voted to start a game! ("..GetGlobalInt("Voteamount").."/"..0.75*table.Count(player.GetAll())..")")
+					v:ChatPrint("Type !start to vote!")
 				end
 			end
 			return ""
--- gamemodes/cheats!/gamemode/ui/teammenu.lua
+++ gamemodes/cheats!/gamemode/ui/teammenu.lua
@@ -8,17 +8,18 @@ end
 
 
 if CLIENT then
+	local function CreateFont()
+		surface.CreateFont( "BFont", {
+			font = "Roboto Cn",
+			extended = true,
+			size = 60,
+			weight = 500,
+			antialias = true
+		})
+	end
+	CreateFont()
 	function TeamMenu()
-		local function CreateFont()
-			surface.CreateFont( "BFont", {
-				font = "Roboto Cn",
-				extended = true,
-				size = 60,
-				weight = 500,
-				antialias = true
-			})
-		end
-		CreateFont()
+
 
 		local frame = vgui.Create( "DFrame" )
 		frame:SetSize( 400, 200 )
@@ -30,6 +31,14 @@ if CLIENT then
 		frame:SetDraggable(false)
 		frame:ShowCloseButton(false)
 		frame:MakePopup()
+		frame.Paint = function()
+			if(!GetGlobalBool("Deathmatch"))then
+				surface.SetDrawColor( 100, 100, 100, 255 )
+				surface.DrawRect(10,(frame:GetTall()/2)-1,frame:GetWide()-20,3)
+			end
+			surface.SetDrawColor( 110, 110, 110, 255 )
+			surface.DrawRect(-4,-4,frame:GetWide()+8,frame:GetTall()+8)
+		end
 
 		if(GetGlobalBool("Deathmatch"))then
 			local DermaButton = vgui.Create("DButton", frame)
@@ -38,36 +47,53 @@ if CLIENT then
 			DermaButton:SetPos(4, 4)
 			DermaButton:SetColor(team.GetColor(3))
 			DermaButton:SetSize(frame:GetWide()-8,frame:GetTall()-8)
+			DermaButton:SetFGColor(Color(255,255,255,0))
 			DermaButton.DoClick = function()
 				net.Start("TEAM")
 				net.WriteInt(3,3)
 				net.SendToServer()
 				frame:Close()
 			end
+			DermaButton.Paint = function()
+				surface.SetDrawColor( 100, 100, 100, 255 )
+				surface.DrawRect(0,0,frame:GetWide(),frame:GetTall())
+			end
 		else
+
+
+
 			local BlueB = vgui.Create("DButton", frame)
 			BlueB:SetText("Join Blue")
 			BlueB:SetFont("BFont")
 			BlueB:SetPos(4, 4)
 			BlueB:SetColor(team.GetColor(2))
-			BlueB:SetSize(frame:GetWide()-8,(frame:GetTall()/2)-4)
+			BlueB:SetSize(frame:GetWide()-8,(frame:GetTall()/2)-5)
 			BlueB.DoClick = function()
 				net.Start("TEAM")
 				net.WriteInt(2,3)
 				net.SendToServer()
 				frame:Close()
 			end
+			BlueB.Paint = function()
+				surface.SetDrawColor( 100, 100, 100, 255 )
+				surface.DrawRect(0,0,frame:GetWide(),frame:GetTall())
+			end
+
 			local RedB = vgui.Create("DButton", frame)
 			RedB:SetText("Join Red")
 			RedB:SetFont("BFont")
-			RedB:SetPos(4,frame:GetTall()/2)
+			RedB:SetPos(4,(frame:GetTall()/2)+1)
 			RedB:SetColor(team.GetColor(1))
-			RedB:SetSize(frame:GetWide()-8,(frame:GetTall()/2)-4)
+			RedB:SetSize(frame:GetWide()-8,(frame:GetTall()/2)-5)
 			RedB.DoClick = function()
 				net.Start("TEAM")
 				net.WriteInt(1,3)
 				net.SendToServer()
 				frame:Close()
+			end
+			RedB.Paint = function()
+				surface.SetDrawColor( 100, 100, 100, 255 )
+				surface.DrawRect(0,0,frame:GetWide(),frame:GetTall())
 			end
 		end
 	end
