6730257178ab8cfe17e665595e5a9841d41dc4ed
W1C0P3E <mineev.0807@gmail.com>
2018-09-21 17:42:26 +0300

    my god what are you doing
    this causes random kicks

--- gamemodes/cheats!/gamemode/misc/anticheat_sv.lua
+++ gamemodes/cheats!/gamemode/misc/anticheat_sv.lua
@@ -1,25 +1,18 @@
 util.AddNetworkString("anticheat")
 
-local anticheatmsg = {
-	"This is not HVH. You do not need to bring your own cheats.",
-	"Cheats gamemode doesn't mean you can use your cheats here.",
-	"Kicked for enabling other cheats.",
-	"Look Stanley, I think perhaps we've gotten off on the wrong foot here."
-}
-
 net.Receive("anticheat", function(len,ply)
 	ply.CheatTimer = CurTime()
 	local bool = net.ReadBool()
 	if (!GetConVar("sv_allowcslua"):GetBool() and bool == true) then
-		ply:Kick(table.Random(anticheatmsg))
+		ply:Kick("This is not HVH. You do not need to bring your own cheats.")
 		print("[CH] "..v:GetName().." has been kicked for cheating")
 	end
 end)
 
 timer.Create("CHAntiCheatTimer",30,0,function() -- if a cheat blocks the anticheat net message or something
 	for k,v in pairs(player.GetAll())do
-		if (CurTime()-v.CheatTimer > 10 and !v:IsBot())then
-			v:Kick(table.Random(anticheatmsg))
+		if (CurTime()-v.CheatTimer > 30 and !v:IsBot())then
+			v:Kick("This is not HVH. You do not need to bring your own cheats.")
 			print("[CH] "..v:GetName().." has been kicked for cheating")
 		end
 	end
